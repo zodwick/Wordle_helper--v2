052a5e497e5004edaf6cd2b1bfaf3892980e573c
zodwick <115868723+zodwick@users.noreply.github.com>
2023-04-01 00:19:01 +0530

    frequency add

--- tester.py
+++ tester.py
@@ -21,33 +21,56 @@
 # print(containsAny('hello', ["p","f"]))
 
 
-def pos_check(word:str, set:list):
-    for i in set:
-        #i = '0a'
-        if word[int(i[0])]!=i[1]:
-            return False
-    return True
+# def pos_check(word:str, set:list):
+#     for i in set:
+#         #i = '0a'
+#         if word[int(i[0])]!=i[1]:
+#             return False
+#     return True
 
 
 # print(pos_check('hello', ['0h', '1l']))
 
 
-words=["hello","heiooo","llovvee"]
-lyellow_with_pos=['1h', '1l']
+# words=["hello","heiooo","llovvee"]
+# lyellow_with_pos=['1h', '1l']
 
 
-def check_individual(word:str, set:str):
-    if word[int(set[0])]!=set[1]:
-            return False
-    return True
+# def check_individual(word:str, set:str):
+#     if word[int(set[0])]!=set[1]:
+#             return False
+#     return True
 
-filter3=[]
+# filter3=[]
     
-for i in list(words):
-        for j in lyellow_with_pos:
+# for i in list(words):
+#         for j in lyellow_with_pos:
             
-             if check_individual(i,j):
-                 filter3.append(i)
+#              if check_individual(i,j):
+#                  filter3.append(i)
                  
-print(filter3)
+# print(filter3)
             
+            
+def frequency(list):
+    string_test=""
+    for i in list:
+        string_test+=i
+    char_freq={}
+
+    for i in string_test:
+        if i in char_freq:
+            char_freq[i]=char_freq[i]+1
+        else:
+            char_freq[i] = 1
+            
+    
+    sorted_frequency = sorted(char_freq.items(), key=lambda x:x[1], reverse=True)
+    sorted_letters=[i[0] for i in sorted_frequency]
+    print(sorted_letters)
+    
+
+
+words=["hello","heiooo","llovvee"]
+
+frequency(words)
